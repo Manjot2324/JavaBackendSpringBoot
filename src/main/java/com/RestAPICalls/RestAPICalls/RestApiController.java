8ea371a9b0838b310026c96a780ab59319283f23
Manjot Singh <Manjot Singh>
2025-04-23 10:41:41 +0530

    Manjot [ADD] UC3.

--- src/main/java/com/RestAPICalls/RestAPICalls/RestApiController.java
+++ src/main/java/com/RestAPICalls/RestAPICalls/RestApiController.java
@@ -1,6 +1,7 @@
 package com.RestAPICalls.RestAPICalls;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,10 @@ public class RestApiController {
     @GetMapping("/query")
     public String getHelloWithName(@RequestParam String name){
         return "Hello" + " " + name + " " + "from Bridze-Labz";
+    }
+    @GetMapping("/param/{name}")
+    public String getHellowithName(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 
 
